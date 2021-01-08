d69c3dd04b5d6b12b5a87eddfbf4dae5067752f9
Guilhermeaug <guilhermeaugustodeoliveira66@gmail.com>
2021-01-07 22:48:45 -0300

    agora kika

--- src/index.js
+++ src/index.js
@@ -11,28 +11,68 @@ client.once("ready", () => { // prints "Ready!" to the console once the bot is o
 });
 
 function random(message) {
-    const number = Math.random(); // generates a random number
-    message.channel.send(number.toString()); // sends a message to the channel with the number
+	const number = Math.random(); // generates a random number
+	message.channel.send(number.toString()); // sends a message to the channel with the number
 }
 
 function leroy(message) {
-    message.channel.send("Leeeeerooooyyyyy JEEEENNNNKINS"); // sends a message to the channel with the number
+	message.channel.send("Leeeeerooooyyyyy JEEEENNNNKINS"); // sends a message to the channel with the number
 }
+
+function lett(message) {
+	message.channel.send("<3");
+}
+
+function thaix(message) {
+	message.channel.send("<3");
+}
+
+function pablo(message) {
+	message.channel.send("AIIII QUE DEEELIIICIA CAARRA");
+}
+
+function eliza(message) {
+	message.channel.send("<3");
+}
+
 
 let commands = new Map();
 commands.set("random", random);
 commands.set("leroy", leroy);
+commands.set("lett", lett);
+commands.set("thaix", thaix);
+commands.set("pablo", pablo);
+commands.set("eliza", eliza);
 
 client.on("message", message => {
-    if (message.content[0] === '?') {
-        const command = message.content.split(" ")[0].substr(1); // gets the command name
-        if (commands.has(command)) { // checks if the map contains the command
-            commands.get(command)(message) // runs the command
+	if (message.content[0] === '?') {
+		const command = message.content.split(" ")[0].substr(1); // gets the command name
+		if (commands.has(command)) { // checks if the map contains the command
+			commands.get(command)(message) // runs the command
 		}
-		if(commands.has(command)){
-			commands.get(command)(message)
-		}
-    }
+
+		if (message.content.startsWith('?chuteLeroi')) {
+			const user = message.mentions.users.first();
+			if (user) {
+			  const member = message.guild.member(user);
+			  if (member) {
+				member
+				  .kick('Optional reason that will display in the audit logs')
+				  .then(() => {
+					message.reply(`Botei o ${user.tag} para mamar com sucesso!`);
+				  })
+				  .catch(err => {
+					message.reply('I was unable to kick the member');
+					console.error(err);
+				  });
+			  } else {
+				message.reply("That user isn't in this guild!");
+			  }
+			} else {
+			  message.reply("You didn't mention the user to kick!");
+			}
+		  }
+	}
 });
 
 
