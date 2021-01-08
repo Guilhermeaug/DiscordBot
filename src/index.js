95c2c46f297dfed7105265fc03d980b34b1e7672
JordanAmaralVicente <jordanitalovicente@gmail.com>
2021-01-08 11:18:31 -0300

    Adicionado o comando '?eu' que mostra frases aleatórias para as pessoas

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const Discord = require("discord.js"); // imports the discord library
+const Discord = require("discord.js");
 const fs = require("fs"); // imports the file io library
 const dotenv = require("dotenv");
 
-const client = new Discord.Client(); // creates a discord client
+const client = new Discord.Client(); 
 //const token = fs.readFileSync("token.txt").toString(); // gets your token from the file
 const envConfig = dotenv.config();
 
 client.once("ready", () => { // prints "Ready!" to the console once the bot is online
-	console.log("Ready!");
+	console.log("Running in Discord!");
 });
 
 function random(message) {
@@ -16,11 +16,7 @@ function random(message) {
 }
 
 function leroy(message) {
-<<<<<<< HEAD
 	message.channel.send("Leeeeerooooyyyyy JEEEENNNNKINS"); // sends a message to the channel with the number
-=======
-    message.channel.send("Leeeeerooooyyyyy JEEENNNNKINS"); // sends a message to the channel with the number
->>>>>>> e6689d888f9369f18d3bd42850cefb197f1ca800
 }
 
 function lett(message) {
@@ -39,8 +35,40 @@ function eliza(message) {
 	message.channel.send("<3");
 }
 
+function kick(message) {
+	const user = message.mentions.users.first();
+	if(user){
+		const user = message.mentions.users.first();
+		if (user) {
+			const member = message.guild.member(user);
+			if (member) {
+			member
+				.kick('Optional reason that will display in the audit logs')
+				.then(() => {
+				message.reply(`Botei o ${user.tag} para mamar com sucesso!`);
+				})
+				.catch(err => {
+				message.reply('I was unable to kick the member');
+				console.error(err);
+				});
+			} else {
+			message.reply("That user isn't in this guild!");
+			}
+		} else message.reply("You didn't mention the user to kick!");
+	}
+}
+
+function randomMessage(message, randomNumber){
+	messages.forEach((item, index)=>{
+		if(index == randomNumber){
+			console.log("index: " + index + " / item: "+ item);
+			message.channel.send(item);
+		}
+	});
+}
 
 let commands = new Map();
+//Comandos com nomes de pessoas entram aqui
 commands.set("random", random);
 commands.set("leroy", leroy);
 commands.set("lett", lett);
@@ -48,36 +76,40 @@ commands.set("thaix", thaix);
 commands.set("pablo", pablo);
 commands.set("eliza", eliza);
 
+let messages = new Array();
+messages.push("Tu é muito estranho");
+messages.push("puta");
+messages.push("nem te conheço");
+messages.push("Para de me chamar");
+messages.push("CHATOOO PRA CARAIO");
+messages.push("Pensei que você tinha morrido");
+messages.push("Não tem mais o que fazer não ?");
+messages.push("Vagabundo(a)");
+messages.push("Lindo(a)");
+messages.push("xuxuzinho");
+messages.push("Bacana");
+messages.push("Bacanudo");
+messages.push("Encapetado");
+messages.push("Estressado");
+messages.push("Corno");
+
 client.on("message", message => {
 	if (message.content[0] === '?') {
 		const command = message.content.split(" ")[0].substr(1); // gets the command name
 		if (commands.has(command)) { // checks if the map contains the command
 			commands.get(command)(message) // runs the command
 		}
-
+		
 		if (message.content.startsWith('?chuteLeroi')) {
-			const user = message.mentions.users.first();
-			if (user) {
-			  const member = message.guild.member(user);
-			  if (member) {
-				member
-				  .kick('Optional reason that will display in the audit logs')
-				  .then(() => {
-					message.reply(`Botei o ${user.tag} para mamar com sucesso!`);
-				  })
-				  .catch(err => {
-					message.reply('I was unable to kick the member');
-					console.error(err);
-				  });
-			  } else {
-				message.reply("That user isn't in this guild!");
-			  }
-			} else {
-			  message.reply("You didn't mention the user to kick!");
-			}
-		  }
+			kick(message);
+		}
+		if(message.content.startsWith('?eu')){
+			let number = Math.round(Math.random() * (messages.length));
+			console.log("Número aletório gerado para mostrar mensagem: " +
+			number);
+			randomMessage(message, number);
+		}
 	}
 });
-
 
 client.login('Nzk2ODQ1NjU4Nzc3OTc2OTAz.X_d2WQ.1SX27DFjx7fwHF9uFVTcIkqorLs'); // starts the bot up
