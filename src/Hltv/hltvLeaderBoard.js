beb820b0f916685bc51bc57b32b7d71d7b0f278f
Lucas Russo <lucasrusso0208@gmail.com>
2021-01-09 17:29:25 -0300

    changing hltv module

---
+++ src/Hltv/hltvLeaderBoard.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const url = "https://www.hltv.org/ranking/teams/";
+const StringBuilder = require("string-builder");
+const sb = new StringBuilder();
+
+const hltvLeaderBoard = axios(url)
+  .then((response, err) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const tabelaStatus = $(".ranking-header");
+    const tabelaTimes = [];
+
+    tabelaStatus.each(function () {
+      const nomeTime = $(this).find(".team-logo > img").attr("title");
+      const posicaoTime = $(this).find(".position").text();
+
+      tabelaTimes.push({
+        nomeTime,
+        posicaoTime,
+      });
+    });
+
+    sb.append("RANKING HLTV 100% ATUALIZADO");
+    sb.appendLine();
+
+    for (let i = 0; i < 10; i++) {
+      sb.appendFormat(
+        "{0} --- {1} {2}",
+        tabelaTimes[i].posicaoTime,
+        tabelaTimes[i].nomeTime
+      );
+      sb.appendLine();
+    }
+
+    message.channel.send(sb.toString());
+  })
+  .catch(console.error);
+
+module.exports = hltvLeaderBoard;
--- src/index.js
+++ src/index.js
@@ -1,76 +1,72 @@
 const Discord = require("discord.js");
-const fs = require("fs"); // imports the file io library
 const ytdl = require("ytdl-core");
-const emoji = require('node-emoji')
-const StringBuilder = require("string-builder");
+const emoji = require("node-emoji");
 require("dotenv").config();
+const hltvLeaderBoard = require("./Hltv/hltvLeaderBoard.js");
 
 const client = new Discord.Client();
 
-//hltv_web_scrap
-const axios = require('axios');
-const cheerio = require('cheerio');
-const url = 'https://www.hltv.org/ranking/teams/';
-
-client.once("ready", () => { // prints "Ready!" to the console once the bot is online
-	console.log("Running in Discord!");
+client.once("ready", () => {
+  console.log("Cabeça tá on");
 });
 
 function random(message) {
-	const number = Math.random(); // generates a random number
-	message.channel.send(number.toString()); // sends a message to the channel with the number
+  const number = Math.random(); // generates a random number
+  message.channel.send(number.toString()); // sends a message to the channel with the number
 }
 
 function leroy(message) {
-	message.channel.send("Leeeeerooooyyyyy JEEENNNNKINS"); // sends a message to the channel with the number
+  message.channel.send("Leeeeerooooyyyyy JEEENNNNKINS"); // sends a message to the channel with the number
 }
 
 function lett(message) {
-	message.channel.send("<3  " + emoji.get('couplekiss') );
+  message.channel.send("<3  " + emoji.get("couplekiss"));
 }
 
 function thaix(message) {
-	message.channel.send("<3  " + emoji.get('heart') + "  " + emoji.get('couplekiss'));
+  message.channel.send(
+    "<3  " + emoji.get("heart") + "  " + emoji.get("couplekiss")
+  );
 }
 
 function pablo(message) {
-	message.channel.send("AIIII QUE DEEELIIICIA CAARRA");
+  message.channel.send("AIIII QUE DEEELIIICIA CAARRA");
 }
 
 function eliza(message) {
-	message.channel.send("<3  " + emoji.get('heart'));
+  message.channel.send("<3  " + emoji.get("heart"));
 }
 
 function kick(message) {
-	const user = message.mentions.users.first();
-	if (user) {
-		const user = message.mentions.users.first();
-		if (user) {
-			const member = message.guild.member(user);
-			if (member) {
-				member
-					.kick('Optional reason that will display in the audit logs')
-					.then(() => {
-						message.reply(`Botei o ${user.tag} para mamar com sucesso!`);
-					})
-					.catch(err => {
-						message.reply('I was unable to kick the member');
-						console.error(err);
-					});
-			} else {
-				message.reply("That user isn't in this guild!");
-			}
-		} else message.reply("You didn't mention the user to kick!");
-	}
+  const user = message.mentions.users.first();
+  if (user) {
+    const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member
+          .kick("Optional reason that will display in the audit logs")
+          .then(() => {
+            message.reply(`Botei o ${user.tag} para mamar com sucesso!`);
+          })
+          .catch((err) => {
+            message.reply("I was unable to kick the member");
+            console.error(err);
+          });
+      } else {
+        message.reply("That user isn't in this guild!");
+      }
+    } else message.reply("You didn't mention the user to kick!");
+  }
 }
 
 function randomMessage(message, randomNumber) {
-	messages.forEach((item, index) => {
-		if (index == randomNumber) {
-			console.log("index: " + index + " / item: " + item);
-			message.channel.send(item);
-		}
-	});
+  messages.forEach((item, index) => {
+    if (index == randomNumber) {
+      console.log("index: " + index + " / item: " + item);
+      message.channel.send(item);
+    }
+  });
 }
 
 let commands = new Map();
@@ -101,107 +97,79 @@ messages.push("Corno");
 
 let queue = new Array();
 
-client.on("message", message => {
-	if (message.content[0] === '?') {
-		const command = message.content.split(" ")[0].substr(1); // gets the command name
-		if (commands.has(command)) { // checks if the map contains the command
-			commands.get(command)(message) // runs the command
-		}
+client.on("message", (message) => {
+  if (message.content[0] === "?") {
+    const command = message.content.split(" ")[0].substr(1); // gets the command name
+    if (commands.has(command)) {
+      // checks if the map contains the command
+      commands.get(command)(message); // runs the command
+    }
 
-		if (message.content.startsWith('?chuteLeroi')) {
-			kick(message);
-		}
-		if (message.content.startsWith('?eu')) {
-			let number = Math.round(Math.random() * (messages.length));
-			console.log("Número aletório gerado para mostrar mensagem: " +
-				number);
-			randomMessage(message, number);
-		}
-		if (message.content.startsWith('?play')) {
-			execute(message);
-		}
-		if (message.content.startsWith('?queue')) {
-			showQueue(message);
-		}
-		if (message.content.startsWith('?teste')) {
-			message.channel.send("TESTADO");
-		}
+    if (message.content.startsWith("?chuteLeroi")) {
+      kick(message);
+    }
+    if (message.content.startsWith("?eu")) {
+      let number = Math.round(Math.random() * messages.length);
+      console.log("Número aletório gerado para mostrar mensagem: " + number);
+      randomMessage(message, number);
+    }
+    if (message.content.startsWith("?play")) {
+      execute(message);
+    }
+    if (message.content.startsWith("?queue")) {
+      showQueue(message);
+    }
+    if (message.content.startsWith("?teste")) {
+      message.channel.send("TESTADO MEU CONSAGRADO");
+    }
 
-		if (message.content.startsWith('?hltv')) {
-			const sb = new StringBuilder();
-			axios(url).then(response => {
-				const html = response.data;
-				const $ = cheerio.load(html);
-				const tabelaStatus = $('.ranking-header');
-				const tabelaTimes = [];
-
-				tabelaStatus.each(function () {
-					const nomeTime = $(this).find('.team-logo > img').attr('title');
-					const posicaoTime = $(this).find('.position').text();
-
-					tabelaTimes.push({
-						nomeTime,
-						posicaoTime
-					});
-				});
-
-				sb.append("RANKING HLTV 100% ATUALIZADO");
-				sb.appendLine();
-
-				for (let i = 0; i < 10; i++) {
-					sb.appendFormat("{0} --- {1} {2}", tabelaTimes[i].posicaoTime, tabelaTimes[i].nomeTime, emoji.get('tongue'));
-					sb.appendLine();
-				}
-
-				message.channel.send(sb.toString());
-
-			}).catch(console.error);
-			
-		}
-
-	}
+    if (message.content.startsWith("?hltv")) {
+      //falta terminar
+    }
+  }
 });
 
 async function execute(message) {
-	const voiceChannel = message.member.voice.channel;
-	if (!voiceChannel) {
-		console.error('No voice channel found');
-		return message.reply('Você não está em um canal de voz, letícia. hehe');
-	}
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel) {
+    console.error("No voice channel found");
+    return message.reply("Você não está em um canal de voz, letícia. hehe");
+  }
 
-	const connection = await voiceChannel.join();
-	const link = message.content.split(" ");
-	const musica = link[1];
+  const connection = await voiceChannel.join();
+  const link = message.content.split(" ");
+  const musica = link[1];
 
-
-	if (queue.length != 0) {
-		queue.push(musica);
-		return message.reply("Sua música entrou na fila, posicao: " + queue.indexOf(musica));
-	} else {
-		queue.push(musica);
-		message.reply("Sua música será executada agora");
-		queue.forEach((item, index) => {
-			console.warn("música : " + item + " Posicao na fila : " + index);
-			const watcher = connection.play(
-				ytdl(item, {
-					filter: 'audioonly',
-					quality: 'lowest',
-				}));
-			watcher.on('end', () => {
-				queue.pop(musica);
-			});
-		})
-	}
-
+  if (queue.length != 0) {
+    queue.push(musica);
+    return message.reply(
+      "Sua música entrou na fila, posicao: " + queue.indexOf(musica)
+    );
+  } else {
+    queue.push(musica);
+    message.reply("Sua música será executada agora");
+    queue.forEach((item, index) => {
+      console.warn("música : " + item + " Posicao na fila : " + index);
+      const watcher = connection.play(
+        ytdl(item, {
+          filter: "audioonly",
+          quality: "lowest",
+        })
+      );
+      watcher.on("end", () => {
+        queue.pop(musica);
+      });
+    });
+  }
 }
 
 function showQueue(message) {
-	if (!queue || queue.length === 0 || queue == null) {
-		message.channel.send("A fila está vazia!");
-	}
-	queue.forEach((item, index) => {
-		message.channel.send((parseInt(index) + 1) + " -  música : " + item);
-	});
+  if (!queue || queue.length === 0 || queue == null) {
+    message.channel.send("A fila está vazia!");
+  }
+  queue.forEach((item, index) => {
+    message.channel.send(parseInt(index) + 1 + " -  música : " + item);
+  });
 }
 
 client.login(process.env.DISCORD_TOKEN); // starts the bot up
