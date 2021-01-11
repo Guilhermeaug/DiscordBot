7e848a283712c906b72635c2aa015bf90063bcd4
Guilhermeaug <guilhermeaugustodeoliveira66@gmail.com>
2021-01-11 13:47:51 -0300

    sandbox puppeter

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ async function searchMusic(message, serverQueue) {
   url = url.concat(keyWords);
 
   let scrape = async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.goto(url);
     const result = await page.evaluate(() => {
