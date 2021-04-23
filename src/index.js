881a37fe4640a6e68e8cffe3da501d29c3ed4002
RussoLucas <lukasrusso22@gmail.com>
2021-04-23 20:50:26 -0300

    refatoração do index.js -remoção de código desnecessário

--- src/index.js
+++ src/index.js
@@ -1,45 +1,78 @@
-const Discord = require("discord.js");
-const emoji = require("node-emoji");
-require("dotenv").config();
+import Discord from "discord.js";
+import emoji from "node-emoji";
+import dotenv from "dotenv";
 
-const hltvLeaderBoard = require("./Hltv/hltvLeaderBoard.js");
-const music = require("./Music/music.js")
+dotenv.config();
+
+// const hltvLeaderBoard = require("./Hltv/hltvLeaderBoard.js");
+// const music = require("./Music/music.js")
 
 const client = new Discord.Client();
 
-const StringBuilder = require("string-builder");
+// const StringBuilder = require("string-builder");
 
 client.once("ready", () => {
   console.log("Vou botar para arrombar!");
 });
 
-function random(message) {
-  const number = Math.random(); // generates a random number
-  message.channel.send(number.toString()); // sends a message to the channel with the number
-}
+const peopleMessages = [
+  {
+    people: "leroy",
+    message: "Leeeeerooooyyyyy JEEENNNNKINS",
+  },
+  {
+    people: "lett",
+    message: `<3 ${emoji.get("couplekiss")}`,
+  },
+  {
+    people: "thaix",
+    message: `<3 ${emoji.get("heart")} ${emoji.get("couplekiss")}`,
+  },
+  {
+    people: "pablo",
+    message: "talarico",
+  },
+  {
+    people: "eliza",
+    message: `<3 ${emoji.get("heart")}`,
+  },
+  {
+    people: "gustavo",
+    message: "tubarão",
+  },
+  {
+    people: "russo",
+    message: "desgraçado",
+  },
+  {
+    people: "jair",
+    message: "nu cê tá fraquin zé",
+  },
+  {
+    people: "anna",
+    message: "balombra",
+  },
+  {
+    people: "guilherme",
+    message: "!play Leno Brega Trepada em Cuiabá",
+  },
+];
 
-function leroy(message) {
-  message.channel.send("Leeeeerooooyyyyy JEEENNNNKINS"); // sends a message to the channel with the number
-}
+const randomMessages = [
+  "Tu é muito estranho",
+  "puta",
+  "nem te conheço",
+  "Para de me chamar",
+  "CHATOOO PRA CARAIO",
+  "Pensei que você tinha morrido",
+  "Flagelado",
+  "Bolsonaro",
+  "Bolsa de colostomia",
+];
 
-function lett(message) {
-  message.channel.send("<3  " + emoji.get("couplekiss"));
-}
-
-function thaix(message) {
-  message.channel.send(
-    "<3  " + emoji.get("heart") + "  " + emoji.get("couplekiss")
-  );
-}
-
-function pablo(message) {
-  message.channel.send("AIIII QUE DEEELIIICIA CAARRA");
-}
-
-function eliza(message) {
-  message.channel.send("<3  " + emoji.get("heart"));
-}
-
+const sendMessage = (message, messageToPeople) => {
+  message.channel.send(messageToPeople);
+};
 
 function kick(message) {
   const user = message.mentions.users.first();
@@ -59,67 +92,36 @@ function kick(message) {
       message.reply("That user isn't in this guild!");
     }
   } else message.reply("You didn't mention the user to kick!");
-
 }
 
-function randomMessage(message, randomNumber) {
-  messages.forEach((item, index) => {
-    if (index == randomNumber) {
-      console.log("index: " + index + " / item: " + item);
-      message.channel.send(item);
-    }
-  });
+function randomMessage(message) {
+  const randomNumber = Math.floor(Math.random() * randomMessages.length);
+  message.channel.send(randomMessages[randomNumber]);
 }
 
-let commands = new Map();
-//Comandos com nomes de pessoas entram aqui
-commands.set("random", random);
-commands.set("leroy", leroy);
-commands.set("lett", lett);
-commands.set("thaix", thaix);
-commands.set("pablo", pablo);
-commands.set("eliza", eliza);
-
-let messages = new Array();
-messages.push("Tu é muito estranho");
-messages.push("puta");
-messages.push("nem te conheço");
-messages.push("Para de me chamar");
-messages.push("CHATOOO PRA CARAIO");
-messages.push("Pensei que você tinha morrido");
-messages.push("Não tem mais o que fazer não ?");
-messages.push("Vagabundo(a)");
-messages.push("Lindo(a)");
-messages.push("xuxuzinho");
-messages.push("Bacana");
-messages.push("Bacanudo");
-messages.push("Encapetado");
-messages.push("Estressado");
-messages.push("Corno");
-
-
-var mensagem = new Discord.Message();
-
+let mensagem = new Discord.Message();
 
 client.on("message", (message) => {
   if (message.author.bot) return;
-  //if (!message.content.startsWith('?')) return;
+  if (!message.content.startsWith("?")) return;
 
-  const serverQueue = music.queue.get(message.guild.id);
+  // const serverQueue = music.queue.get(message.guild.id);
 
-  const command = message.content.split(" ")[0].substr(1); // gets the command name
-  if (commands.has(command)) {
-    // checks if the map contains the command
-    commands.get(command)(message); // runs the command
+  const content = message.content.split(" ")[0].substr(1); // gets the command name
+
+  if (content !== "") {
+    peopleMessages.map((el) => {
+      if (el.people === content) {
+        sendMessage(message, el.message);
+      }
+    });
   }
 
   if (message.content.startsWith("?chuteLeroi")) {
     kick(message);
   }
   if (message.content.startsWith("?eu")) {
-    let number = Math.round(Math.random() * messages.length);
-    console.log("Número aletório gerado para mostrar mensagem: " + number);
-    randomMessage(message, number);
+    randomMessage(message);
   }
 
   if (message.content.startsWith("?play")) {
@@ -153,22 +155,24 @@ client.on("message", (message) => {
     music.ClearQueue(message, serverQueue);
   }
 
-  if (!message.content.startsWith("?") && mensagem.content != '') {
+  if (!message.content.startsWith("?") && mensagem.content !== "") {
     if (message.author === mensagem.author) {
       music.Execute(message, serverQueue, music.listaVideos);
-      mensagem.content = '';
+      mensagem.content = "";
     }
   }
 
   if (message.content.startsWith("?teste")) {
-    message.channel.send(`Tô pronto para arrombar! {0} {1}`, emoji.get('white_check_mark'), emoji.get('dragon_face'));
+    message.channel.send(
+      `Tô pronto para arrombar! {0} {1}`,
+      emoji.get("white_check_mark"),
+      emoji.get("dragon_face")
+    );
   }
 
   if (message.content.startsWith("?hltv")) {
     hltvLeaderBoard.Ranking(message);
   }
-
 });
 
-
-client.login(process.env.DISCORD_TOKEN); // starts the bot up
+client.login(process.env.DISCORD_TOKEN);
