b1dc5fb44e4c26f8a867df3890dbd0eeb37d5e2a
Guilherme Augusto de Oliveira <guilhermeaugustodeoliveira66@gmail.com>
2021-04-25 16:55:48 -0300

    Update hltvLeaderBoard.js

--- src/Hltv/hltvLeaderBoard.js
+++ src/Hltv/hltvLeaderBoard.js
@@ -5,7 +5,7 @@ import puppeteer from "puppeteer";
 const url = "https://www.hltv.org/ranking/teams/";
 
 const scrape = async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.goto(url);
 
